3d13dae1bba6d983d3570127d68b0a6352e4f59c
Esteban Quintana <estebanquintana6@gmail.com>
2023-01-30 17:00:53 -0600

    [EVENTS] ES6 async style

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -7,18 +7,10 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
-  last_name: {
-    type: String,
-    required: true,
-  },
   email: {
     type: String,
     required: false,
     lowercase: true
-  },
-  telephone: {
-    type: String,
-    required: true,
   },
   password: {
     type: String,
