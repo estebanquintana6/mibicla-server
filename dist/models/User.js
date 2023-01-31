15c5befac44d7fc5885efa5451ba10d9b075c57b
Esteban Quintana <estebanquintana6@gmail.com>
2023-01-31 17:45:44 -0600

    Moved server folder

---
+++ dist/models/User.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const { Schema } = mongoose_1.default;
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: false,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+});
+const Users = mongoose_1.default.model('Users', UserSchema);
+exports.default = Users;
--- server/dist/utils/validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validatePassword = void 0;
-const validatePassword = (password) => {
-    return password;
-};
-exports.validatePassword = validatePassword;
